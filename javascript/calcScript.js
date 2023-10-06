b6c1d19f9e3735e1c3512a6666d6625a3eb05614
np2029 <np2029@hw.ac.uk>
2023-10-06 10:54:38 +0100

    implemented calc buttons

--- javascript/calcScript.js
+++ javascript/calcScript.js
@@ -1,4 +1,39 @@
-for (let x=1;x<10;x++) {
+var displayText = "0";
+var calc = "";
+
+//numpad
+var numArr = [];
+for (let x=1;x<11;x++) {
     var y = $("<div></div>").addClass("numButton");
+    y.html("<p>"+x+"</p>");
     $("#numpad").append(y);
+    y.click(function() {
+        numPress(x);
+    });
+    numArr[x] = y;
+}
+
+//change 10 to 0
+numArr[0] = numArr[10];
+delete(numArr[10]);
+console.log(numArr);
+numArr[0].text("0");
+
+
+//operator buttons
+let oppArr = ["+","-","*","/"];
+for (let x=0;x<oppArr.length;x++) {
+    let tempEle = $("<div></div>");
+    tempEle.html("<p>"+oppArr[x]+"</p>");
+    tempEle.click(function() {
+        numPress(oppArr[x]);
+    })
+    $("#ops").append(tempEle);
+}
+
+//add something to calculation
+function numPress(val) {
+    calc += val;
+    displayText = calc;
+    $("#display").text(displayText);
 }
