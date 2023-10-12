ef7d6d37f8766787179b926757f7c4ef4abc0e4c
np2029 <np2029@hw.ac.uk>
2023-10-12 10:12:17 +0100

    rss stuff

---
+++ javascript/stupid.js
@@ -0,0 +1,37 @@
+async function test() {
+    let prom = await fetch( 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml' );
+    let text = await prom.text();
+
+    // console.log( text.substring(0, 100 ) );
+
+    
+    let parser = new DOMParser();
+    let xmlDoc = parser.parseFromString(text, "text/xml");
+
+    // console.log( xmlDoc );
+
+    const items = xmlDoc.querySelectorAll("item");
+    // console.log('items:', items );
+
+    items.forEach( (el)=> {
+        let title       = el.querySelector('title').textContent;
+        let description = el.querySelector('description').textContent;
+        let guid        = el.querySelector('guid').textContent;
+
+        // console.log('title:',       title       );
+        // console.log('description:', description);
+        // console.log('guid:',        guid);
+
+        let div = document.createElement('div');
+        document.getElementById('rssStuff').appendChild(div);
+        div.classList.add('rssBox');   
+        div.innerHTML = `
+            <h2>${title}</h2>
+            <p>${description}</p>
+        `;
+        if ( guid ) div.innerHTML += `<a href='${guid}'>LINK</a>`;
+
+    } )
+}
+
+test();
